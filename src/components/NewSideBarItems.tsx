ce524b6605cc8844b5757651424287909aa23f0f
sa'd <asaad5436@gmail.com>
2024-05-04 17:12:06 +0500

    fix: dynamic tab selection based on the usePathname hook

--- src/components/NewSideBarItems.tsx
+++ src/components/NewSideBarItems.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link"
 import {Tooltip,TooltipContent,TooltipTrigger,TooltipProvider} from "@/components/ui/tooltip"
 import { useState } from "react"
+import { usePathname } from 'next/navigation'
 import {
   Package,
   Package2,
@@ -15,8 +16,8 @@ import {
 
 export default function NewSideBarItems(){
 
-const [path, setPath] = useState("dashboard");
-
+const currentPath = usePathname();
+const [path, setPath] = useState(`${currentPath.split("/")[1]}`);
 return(
 <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
         <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
@@ -49,10 +50,10 @@ return(
                 className={`flex h-9 w-9 items-center justify-center rounded-lg ${path === "inventory" ? "bg-accent text-accent-foreground" : "text-muted-foreground"}transition-colors hover:text-foreground md:h-8 md:w-8`}
               onClick={()=>setPath("inventory")}>
                 <ShoppingCart className="h-5 w-5" />
-                <span className="sr-only">Orders</span>
+                <span className="sr-only">Inventory</span>
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="right">Orders</TooltipContent>
+            <TooltipContent side="right">Inventory</TooltipContent>
           </Tooltip>
           </TooltipProvider>
           <TooltipProvider>
@@ -63,10 +64,10 @@ return(
                 className={`flex h-9 w-9 items-center justify-center rounded-lg  ${path === "account" ? "bg-accent text-accent-foreground" : "text-muted-foreground"} transition-colors hover:text-foreground md:h-8 md:w-8`}
               onClick={()=>setPath("account")}>
                 <Package className="h-5 w-5" />
-                <span className="sr-only">Products</span>
+                <span className="sr-only">Account</span>
               </Link>
             </TooltipTrigger>
-            <TooltipContent side="right">Products</TooltipContent>
+            <TooltipContent side="right">Account</TooltipContent>
           </Tooltip>
           </TooltipProvider>
         </nav>

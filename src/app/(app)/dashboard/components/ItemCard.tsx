f9d18cbb86c845b3288778791e2ccd42c96a9718
sa'd <asaad5436@gmail.com>
2024-05-10 01:51:27 +0500

    feat: add discount tags on item card

--- src/app/(app)/dashboard/components/ItemCard.tsx
+++ src/app/(app)/dashboard/components/ItemCard.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import Image from 'next/image';
 import pic from '@/images/orange1.png'
-
+import { BadgeCent } from "lucide-react";
 
 interface props{
   name: string;
@@ -10,7 +10,11 @@ interface props{
   price: string;
 }
 const ItemCard: React.FC<props> = (props) =>  {
-  return <Button variant="outline" className="h-[200px] w-[15%] ml-4 mb-3 justify-between flex flex-col">
+  return <Button variant="outline" className="relative h-[200px] w-[15%] ml-4 mb-3 justify-between flex flex-col">
+  <div className="absolute origin-bottom-left left-4 -top-2 -rotate-45 w-[40px] h-[40px]">
+  <BadgeCent className="absolute"/>
+  <span className="absolute text-[11px] top-7 -left-3">10% off</span>
+  </div>
   <div> {props['name']} </div>
   <span>{`Price $${props['price']}`}</span>
   <Image src={pic} alt="chocolaty" width={50} height={50}/>

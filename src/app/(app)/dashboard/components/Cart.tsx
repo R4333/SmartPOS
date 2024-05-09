d9e2fffbe5a00b68ef7e40468071010c62feec6b
sa'd <asaad5436@gmail.com>
2024-05-10 01:12:21 +0500

    fix: cart height

--- src/app/(app)/dashboard/components/Cart.tsx
+++ src/app/(app)/dashboard/components/Cart.tsx
@@ -111,16 +111,6 @@ export default function Cart(){
                   <div className="font-semibold">Customer Information</div>
                   <dl className="grid gap-3">
                     <div className="flex items-center justify-between">
-                      <dt className="text-muted-foreground">Customer</dt>
-                      <dd>Liam Johnson</dd>
-                    </div>
-                    <div className="flex items-center justify-between">
-                      <dt className="text-muted-foreground">Email</dt>
-                      <dd>
-                        <a href="mailto:">liam@acme.com</a>
-                      </dd>
-                    </div>
-                    <div className="flex items-center justify-between">
                       <dt className="text-muted-foreground">Phone</dt>
                       <dd>
                         <a href="tel:">+1 234 567 890</a>
--- src/app/(app)/inventory/page.tsx
+++ src/app/(app)/inventory/page.tsx
@@ -1,72 +1,9 @@
-import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  CardContent,
-  CardFooter,
-  Card,
-} from "@/components/ui/card";
-
-import {
-  TableHead,
-  TableRow,
-  TableHeader,
-  TableBody,
-  Table,
-} from "@/components/ui/table";
-
-import BodyTable from "./components/BodyTable";
-
-import AddItem from "./components/AddItem";
-
-import { ScrollArea } from "@/components/ui/scroll-area";
-
+import CardComponent from "./components/CardComponent";
 
 export default function Component() {
 
   return (
-    <Card className="w-[97.5%] ml-5 pr-4 pl-4">
-      <CardHeader className="pt-5">
-        <CardTitle className="mb-4">Inventory</CardTitle>
-        <CardDescription className="flex justify-between">
-         Manage your items and their details 
-          <div className="mr-9">
-          <AddItem />
-          </div>
-        </CardDescription>
-      </CardHeader>
-      <ScrollArea className="h-[750px] w-full rounded-md border p-4">
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="hidden w-[100px] sm:table-cell">
-                <span className="sr-only">Image</span>
-              </TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead className="hidden md:table-cell">Price</TableHead>
-              <TableHead className="hidden md:table-cell">
-                Total Sales
-              </TableHead>
-              <TableHead className="hidden md:table-cell">Created at</TableHead>
-              <TableHead>
-                <span className="sr-only">Actions</span>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <BodyTable />
-        </Table>
-      </CardContent>
-      </ScrollArea>
-      <CardFooter>
-        <div className="text-xs text-muted-foreground mt-2">
-          Showing
-          <strong>1-10</strong> of <strong>32</strong>
-          products{"\n              "}
-        </div>
-      </CardFooter>
-    </Card>
+    <CardComponent/>
   );
 }
 

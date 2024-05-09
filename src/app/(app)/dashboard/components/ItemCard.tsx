b64ccc594cc4745f23a777f9fb92260dd22032a3
sa'd <asaad5436@gmail.com>
2024-05-10 03:34:02 +0500

    style: itemcard styling

--- src/app/(app)/dashboard/components/ItemCard.tsx
+++ src/app/(app)/dashboard/components/ItemCard.tsx
@@ -10,16 +10,21 @@ interface props{
   price: string;
 }
 const ItemCard: React.FC<props> = (props) =>  {
-  return <Button variant="outline" className="relative h-[200px] w-[15%] ml-4 mb-3 justify-between flex flex-col">
-  <div className="absolute origin-bottom-left left-4 -top-2 -rotate-45 w-[40px] h-[40px]">
-  <BadgeCent className="absolute"/>
-  <span className="absolute text-[11px] top-7 -left-3">10% off</span>
-  </div>
-  <div> {props['name']} </div>
-  <span>{`Price $${props['price']}`}</span>
-  <Image src={pic} alt="chocolaty" width={50} height={50}/>
+  return( <Button variant="outline" className="relative h-[200px] w-[18%] ml-4 mb-3 flex justify-between">
+              <div className="absolute origin-bottom-left left-4 -top-2 -rotate-45 w-[40px] h-[40px]">
+                    <BadgeCent className="absolute"/>
+                    <span className="absolute text-[11px] top-7 -left-3">10% off</span>
+              </div>
+              <div className="pl-2 flex flex-col h-[80%] items-start justify-end">
+                    <div className="mb-16 flex flex-col items-start text-wrap text-start"> {props['name']}
 
-  </Button>
+                    <span className="text-xs text-slate-400">{`#${props['barcode']}`}</span>
+                    </div>
+                    <span className="">{`$${props['price']}`}</span>
+              </div>
+              <Image src={pic} alt="chocolaty" width={70} height={90} className="self-end mb-6"/>
+        </Button>
+        )
 }
 
 export default ItemCard;
